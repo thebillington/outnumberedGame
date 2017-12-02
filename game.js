47a1d17ad09ab6ad0989d77d63bcc6b368b11af1
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2017-12-02 07:28:59 +0000

    Clear the canvas every frame and add background colour.

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
-// Create a rvariable to hold some shapes
+// Create a variable to hold some shapes
 var r;
 var c;
+
+// Variable to hold background colour
+var bgColour;
 
 // Function ran at the start of the game
 function setup() {
@@ -12,13 +15,22 @@ function setup() {
     r = Rectangle(0, 0, 100, 100, 0, color(255,0,0));
     c = Circle(100, 100, 50, 0, color(0,150,150));
     
+    // Set the background colour
+    bgColour = color(255, 255, 255);
+    
 }
 
 // Render function
 function draw() {
     
+    // Clear the canvas
+    background(bgColour);
+    
     //Draw the shapes
     drawRect(r);
     drawCircle(c);
+    
+    // Rotate the rectangle
+    r.rotation++;
     
 }
